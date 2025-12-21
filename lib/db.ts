777655cc8688b77b7e8c2900c2bf37e1d44b219b
shivam829 <shivamdangi829@gmail.com>
2025-12-22 05:09:42 +0530

    10

--- lib/db.ts
+++ lib/db.ts
@@ -3,20 +3,35 @@ import mongoose from "mongoose";
 const MONGODB_URI = process.env.MONGODB_URI!;
 
 if (!MONGODB_URI) {
-  throw new Error("Please define MONGODB_URI");
+  throw new Error("Please define MONGODB_URI in environment variables");
 }
 
-let cached = (global as any).mongoose || { conn: null, promise: null };
+// Global cache (VERY important for Vercel)
+let cached = (global as any).mongoose;
 
-const connectDB = async () => {
-  if (cached.conn) return cached.conn;
+if (!cached) {
+  cached = (global as any).mongoose = {
+    conn: null,
+    promise: null,
+  };
+}
+
+async function dbConnect() {
+  if (cached.conn) {
+    return cached.conn;
+  }
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => mongoose);
+    cached.promise = mongoose
+      .connect(MONGODB_URI, {
+        bufferCommands: false,
+        maxPoolSize: 10,
+      })
+      .then((mongoose) => mongoose);
   }
 
   cached.conn = await cached.promise;
   return cached.conn;
-};
+}
 
-export default connectDB;
+export default dbConnect;
