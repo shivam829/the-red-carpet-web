d68af78064f1ddece6b0c21ff189e5e73ce6b763
shivam829 <shivamdangi829@gmail.com>
2025-12-22 06:02:57 +0530

    19

--- lib/db.ts
+++ lib/db.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const MONGODB_URI = process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
-  throw new Error("MONGODB_URI is not defined");
+  throw new Error("MONGODB_URI not defined");
 }
 
 let cached = (global as any).mongoose;
